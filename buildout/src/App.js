5159f470b1caf5ed09d41ce24495e8bac87add16
Mahima <priyanshisrivastava708016@gmail.com>
2025-06-06 16:00:39 +0530

    ui

--- buildout/src/App.js
+++ buildout/src/App.js
@@ -1,7 +1,7 @@
 import Calculator from "./Calculator/Calculator";
 function App() {
   return (
-    <div>
+    <div style={{height:"100vh",width:"100vw",display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column",border:"1px solid green"}}>
      <Calculator/>
     </div>
   );
--- buildout/src/Calculator/Calculator.jsx
+++ buildout/src/Calculator/Calculator.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
+import styles from "./Calculator.module.css";
 export default function Calculator(){
+    const arr = ["7","8","9","+","4","5","6","-","1","2","3","*","C","0","=","/"];
+    const [userInput,setUserInput] = useState("");
+    console.log(userInput);
     return(
-        <div>
-            <h1>Hello</h1>
+        <div style={{height:"80vh",width:"100vh",display:"flex",alignItems:"center",flexDirection:"column",border:"1px solid blue"}}>
+            <h1>React Calculator</h1>
+            <div style={{width:"100%",display:"flex",alignContent:"center",justifyContent:"center"}}>
+                <input placeholder=""  className={styles.input} onChange={(e)=>setUserInput(e.target.value)}/>
+            </div>
+            <div className={styles.container}>
+            {arr.map((item,idx)=>(
+                <button key={idx} className={styles.button}>{item}</button>
+            ))}
+            </div>
         </div>
     )
 }
